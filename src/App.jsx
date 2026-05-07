a170bf7fc9841625aa3e99ca7edb749e3283eadc
Nate <natezhang44@gmail.com>
2026-05-07 14:04:14 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { useEffect } from "react";
 import GenderBiasCourseSelectionWebsite from "./GenderBiasCourseSelectionWebsite";
 
 export default function App() {
+  useEffect(() => {
+    if ("scrollRestoration" in window.history) {
+      window.history.scrollRestoration = "manual";
+    }
+
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    });
+  }, []);
+
   return <GenderBiasCourseSelectionWebsite />;
 }
